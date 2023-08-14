f90bf93cc879965a7c8f39eec284627d47b594ee
Arran Stevens <55632520+NoodlePlexium@users.noreply.github.com>
2023-08-14 12:42:43 +0200

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-class SermonCentralText:
+class SermonCentralScraper:
 
     def getText(self, book, chapter):
 
@@ -44,4 +44,53 @@ class SermonCentralText:
             return None
 
 
+class YouVersionScraper:
+
+    def __init__(self):
+        self.bookAbreviation = {
+            "Genesis" : "GEN",
+            "Exodus" : "EXO",
+            "Leviticus" : "LEV",
+            "Numbers" : "NUM",
+            "Deuteronomy" : "DEU",
+            "Joshua" : "JOS",
+            "Judges" : "JDG",
+            "Ruth" : "RUT"
+        }
+
+
+    def getText(self, book, chapter):
+
+        # GET HTML CONTENT
+        url = "https://www.bible.com/bible/111/"+ self.bookAbreviation[book] +"." + str(chapter) + ".NIV"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+
+            # GET HTML
+            soup = BeautifulSoup(response.content, "html.parser")
+            bible_content = soup.find("div", class_="ChapterContent_chapter__uvbXo")
+            span_elements = bible_content.find_all("span")
+
+            # KEEP CERTAIN ELEMENTS
+            keep_elements = []
+            for element in span_elements:
+                eClass = element.get("class")[0]
+                
+                if eClass == "ChapterContent_content__RrUqA" or eClass == "ChapterContent_label__R2PLt" or eClass == "ChapterContent_heading__xBDcs":
+                    keep_elements.append(element)
+
+            # GENERATE TEXT STRING
+            text = ""
+            for element in keep_elements:
+                text += element.get_text() + "\n"
+
+            return text
+
+        else:
+            print("Failed to retrieve the webpage")
+            return None
+
+
+        
 
