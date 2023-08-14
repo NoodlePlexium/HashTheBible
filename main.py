0fe22a0bf3cc7f1e6a0f8a1de8f630876ed9f61d
Arran Stevens <55632520+NoodlePlexium@users.noreply.github.com>
2023-08-14 14:01:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def cleanRawText(rawText):
     cleanText = ps.remove_space_duplicates(cleanText, " ")
     cleanText = ps.insert_spaces_between_numbers_and_words(cleanText)
     cleanText = ps.remove_spaces_before(cleanText, [".", '”', "!", "?", ",", "’", ":", ";", ")"])
+    cleanText = ps.remove_spaces_after(cleanText, ["(", "“"])
 
     return cleanText
 
@@ -19,26 +20,30 @@ def cleanRawText(rawText):
 youText = scraper.YouVersionScraper()
 sermonText = scraper.SermonCentralScraper()
 
-for i in range(1, 50):
+book = "Judges"
+
+for i in range(21):
 
     # YouText Text Processing
-    rawText1 = youText.getText("Genesis", i)
+    rawText1 = youText.getText(book, i+1)
     cleanText1 = cleanRawText(rawText1)
 
     # SermonText Text Processing
-    rawText2 = sermonText.getText("Genesis", i)
+    rawText2 = sermonText.getText(book, i+1)
     cleanText2 = cleanRawText(rawText2)
 
     # Console output
     hash1 = sha256(cleanText1.encode('utf-8')).hexdigest()
     hash2 = sha256(cleanText2.encode('utf-8')).hexdigest()
-    print(hash1)
-  
 
     if (hash1 != hash2):
-        print(f"Genesis {i}")
+        print(f"{book} {i+1}")
         print(hash1)
         print(hash2)   
         print(cleanText1)
         print(cleanText2)
         print("\n")
+
+    else:
+        print(hash1)   
+
