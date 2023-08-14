f66ae1e9b656634a365d704e9158b0a901cca9f1
Arran Stevens <55632520+NoodlePlexium@users.noreply.github.com>
2023-08-14 12:42:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,42 @@ import processor as ps
 import webscraper as scraper
 
 
-sermonText = scraper.SermonCentralText()
+def cleanRawText(rawText):
+    cleanText = ""
+    cleanText = rawText.replace('\n', ' ')
+    cleanText = ps.capitalize_all(cleanText)
+    cleanText = ps.whitelist_symbols(cleanText, [".", '”', "“", "!", "?", ",", "’", ":", ";", "(", ")"])
+    cleanText = ps.remove_footnotes(cleanText)
+    cleanText = ps.remove_space_duplicates(cleanText, " ")
+    cleanText = ps.insert_spaces_between_numbers_and_words(cleanText)
+    cleanText = ps.remove_spaces_before(cleanText, [".", '”', "!", "?", ",", "’", ":", ";", ")"])
+
+    return cleanText
+
+# Webscraper class
+youText = scraper.YouVersionScraper()
+sermonText = scraper.SermonCentralScraper()
 
 for i in range(1, 50):
 
-    rawInput = sermonText.getText("Genesis", i)
-    cleanText = ""
+    # YouText Text Processing
+    rawText1 = youText.getText("Genesis", i)
+    cleanText1 = cleanRawText(rawText1)
 
-    # Remove line breaks and spaces
-    cleanText = rawInput.replace('\n', ' ')
-    cleanText = cleanText.replace(' ', '_')
-    cleanText = ps.remove_footnote_brackets(cleanText)
-    cleanText = ps.remove_space_duplicates(cleanText, "_")
+    # SermonText Text Processing
+    rawText2 = sermonText.getText("Genesis", i)
+    cleanText2 = cleanRawText(rawText2)
 
-    print(sha256(cleanText.encode('utf-8')).hexdigest())
+    # Console output
+    hash1 = sha256(cleanText1.encode('utf-8')).hexdigest()
+    hash2 = sha256(cleanText2.encode('utf-8')).hexdigest()
+    print(hash1)
+  
+
+    if (hash1 != hash2):
+        print(f"Genesis {i}")
+        print(hash1)
+        print(hash2)   
+        print(cleanText1)
+        print(cleanText2)
+        print("\n")
