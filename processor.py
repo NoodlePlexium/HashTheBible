be3818b749cd6be9589bd0821d76d710b5fda4a7
Arran Stevens <55632520+NoodlePlexium@users.noreply.github.com>
2023-08-14 12:42:58 +0200

    Update processor.py

--- processor.py
+++ processor.py
@@ -10,7 +10,7 @@ def replace_character_at_index(input_string, index, new_character):
     
     return input_string[:index] + new_character + input_string[index + 1:]
 
-def remove_footnote_brackets(input_string):
+def remove_footnotes(input_string):
     i = 0
     removeMode = False
     
@@ -27,6 +27,62 @@ def remove_footnote_brackets(input_string):
             i-=1
 
     return input_string
+
+def capitalize_all(input_string):
+    capitalized_string = ""
+    for i in range(len(input_string)):
+        capitalized_string += input_string[i].upper()
+    return capitalized_string
+ 
+
+def whitelist_symbols(input_string, whitelisted):
+    cleaned_string = ""
+
+    i=0
+    while i < len(input_string):
+        char = input_string[i]
+        
+        if char.isalnum() or char == " ":
+            cleaned_string += char
+        else:
+            for j in range(len(whitelisted))  :
+                if char == whitelisted[j]:
+                    cleaned_string += char
+
+        i += 1                
+
+    return cleaned_string 
+
+
+
+
+def remove_spaces_before(input_string, chars):
+    cleaned_string = ""
+
+    i=0
+    while i < len(input_string) - 1:
+        char = input_string[i]
+        nextChar = input_string[i+1]
+        
+        added = False
+        for j in range(len(chars)):
+
+            if nextChar == chars[j]:
+                if char == " ":
+                   added = True
+                else:
+                    cleaned_string += char
+                    added = True
+
+        if not added:
+            cleaned_string += char    
+
+
+        i += 1                
+
+    return cleaned_string 
+
+
 
 
 def remove_space_duplicates(input_string, spaceChar):
@@ -50,5 +106,33 @@ def remove_space_duplicates(input_string, spaceChar):
     # last character cannot be a space
     if (cleaned_string[len(cleaned_string)-1]==spaceChar):
         cleaned_string = cleaned_string[:-1]    
+
+    return cleaned_string
+
+
+
+
+def insert_spaces_between_numbers_and_words(input_string):
+
+    cleaned_string = ""
+    wasANumber = False
+
+    i=0
+    while i < len(input_string):
+        char = input_string[i]
+
+        if char.isnumeric():
+            wasANumber = True
+            cleaned_string += char
+
+        else:
+            if wasANumber and char != " ":
+                # insert space
+                cleaned_string+= " "
+
+            cleaned_string += char
+            wasANumber = False    
+
+        i += 1
 
     return cleaned_string
